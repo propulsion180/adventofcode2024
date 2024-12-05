5813d8790d0412f9be0d96e3de1c3bfa7fe409f6
propulsion180 <wmmperera@tuta.io>
2024-12-05 23:33:03 +1300

    day5. Can't believe that I completed it that was so confusing

---
+++ day5/p1/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+func is_valid(rules map[int][]int, ruleInv map[int][]int, update []int) bool {
+
+	for i, val := range update {
+		before := update[:i]
+		after := update[i+1:]
+
+		beforeRule := ruleInv[val]
+		afterRule := rules[val]
+
+		for _, b := range before {
+			if !slices.Contains(beforeRule, b) {
+				return false
+			}
+		}
+
+		for _, a := range after {
+			if !slices.Contains(afterRule, a) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func MiddleElement(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	middleIndex := len(nums) / 2
+	return nums[middleIndex]
+}
+
+func main() {
+	rules := make(map[int][]int)
+	rulesInv := make(map[int][]int)
+	var updates [][]int
+
+	file, err := os.Open("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	collectRules := true
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if collectRules {
+			if line == "" {
+				collectRules = false
+				continue
+			}
+			parts := strings.Split(line, "|")
+			if len(parts) < 2 {
+				log.Fatal("Error in gettign rules")
+			}
+			one, err := strconv.Atoi(parts[0])
+			if err != nil {
+				log.Fatal("Couldn't convert first part to int", err)
+			}
+
+			two, err := strconv.Atoi(parts[1])
+			if err != nil {
+				log.Fatal("Couldn't convert second part to int", err)
+			}
+
+			rulesInv[two] = append(rulesInv[two], one) //so that when you find something you can see what was meant to be before it
+			rules[one] = append(rules[one], two)
+		} else {
+			var temp []int
+			stringInts := strings.Split(line, ",")
+			for _, val := range stringInts {
+				int, err := strconv.Atoi(val)
+				if err != nil {
+					log.Fatal("Couldn't convert update string to int", err)
+				}
+				temp = append(temp, int)
+			}
+			updates = append(updates, temp)
+		}
+	}
+	fmt.Println("Collected")
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	totalMiddles := 0
+
+	fmt.Println("Checking for validity")
+
+	for _, val := range updates {
+		if is_valid(rules, rulesInv, val) {
+			fmt.Println(val)
+			totalMiddles = totalMiddles + MiddleElement(val)
+		}
+	}
+
+	fmt.Println(totalMiddles)
+}
